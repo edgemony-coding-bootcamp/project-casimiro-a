522767a9f3c7b8561c04bfeed41f8b3898900bdb
Agnese Spinella <agnesespinella@gmail.com>
2022-02-08 18:22:18 +0100

    header style fixed

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,37 +1,32 @@
 import styles from './index.module.scss'
 import Link from "next/link"
 
-export default function Header({children}) {
-    return(
-    <>
-    <div className={styles.wrapper}>
-        <nav className={styles.nav}>
-            <img className={styles.logo} src="/logo.png" alt="logo"></img>
-            <ul>
-              <li>
-                  <Link href="/città">
-                      <a>Città</a>
-                  </Link>
-              </li>  
-            </ul>
-            <ul>
-              <li>
-                  <Link href="/esperienze">
-                      <a>Esperienze</a>
-                  </Link>
-              </li>  
-            </ul>
-            <ul>
-              <li>
-                  <Link href="/about">
-                      <a>About</a>
-                  </Link>
-              </li>  
-            </ul>
+export default function Header({ children }) {
+    return (
+        <div className={styles.wrapper_header}>
+            <img src="/logo.png" alt="logo"></img>
+            <nav className={styles.nav}>
+                <ul>
+                    <li>
+                        <Link href="/città">
+                            <a>Città</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/esperienze">
+                            <a>Esperienze</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a>About</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
             <div className={styles.search}>
-            <input  type="text" placeholder="Type to Search..." className={styles.inputSearch}></input>
+                <input type="text" placeholder="Type to Search..." className={styles.inputSearch}></input>
             </div>
-        </nav>
-    </div>
-    </>)
+        </div>
+    )
 }

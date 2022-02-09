8dd3fe6f128fd36e20a497d0408b332996e5970c
Dario Castiglione <dario.castiglione99@gmail.com>
2022-02-09 15:30:21 +0100

    fix sidebar

--- components/SidebarHeroDot/SidebarHeroDot.jsx
+++ components/SidebarHeroDot/SidebarHeroDot.jsx
@@ -4,10 +4,11 @@ export default function SidebarHeroDot({ isChosen }) {
   let styled = {};
 
   isChosen
-    ? (styled = { width: "40px", height: "40px" })
+    ? (styled = { width: "40px", height: "40px", opacity: "0.9" })
     : (styled = {
         width: "20px",
         height: "20px",
+
       });
 
   return <div className={styles.container} style={styled}></div>;

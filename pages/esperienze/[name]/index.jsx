965a30cc9b37a9f707f78fa6eec4287731e49ac6
Roberta Pennisi <roberta.pennisi97@gmail.com>
2022-02-10 11:34:13 +0100

    fixed experience

--- pages/esperienze/[name]/index.jsx
+++ pages/esperienze/[name]/index.jsx
@@ -1,27 +1,23 @@
 import styles from './SingleExperience.module.scss'
-import Header from '../../../components/Header'
+
 import HeroIntern from "../../../components/HeroIntern";
 import Activities from '../../../components/Activities';
 import CitiesGrid from '../../../components/CitiesGrid';
-import Footer from '../../../components/Footer'
+
 import Layout from '../../../components/Layouts';
 //dati per esempio
 const data = {
-    name: "Amsterdam",
-    content: "Amsterdam è la capitale dell'Olanda, centro culturale e una delle mete preferite in Europa. Pur non essendo una città enorme, le sue caratteristiche peculiari attirano milioni di turisti per escursioni e lunghe vacanze.\nCostruita sotto il livello del mare intorno all'anno 1000, dopo una bonifica della zona, Amsterdam è nota come la “Venezia del Nord”, per i suoi numerosi canali.Amatissima dai giovani di ogni parte del mondo, che vengono in questa città per la sua folle vita notturna e per immergersi nel celebre mondo dei trasgressivi “coffe shops” e del noto quartiere a luci rosse, Amsterdam attrae anche gli appassionati d'arte, che sono attirati dai suoi ricchi musei e dalla sua raffinata architettura. Qui predomina, in particolare, lo stile rinascimentale, profuso in Olanda nel XVII secolo, che attribuisce alla città un’atmosfera davvero unica.",
-    cover_image_url: "https://images-sandbox.musement.com/cover/0002/15/amsterdam_header-114429.jpeg"
+    name: "Passeggiate e degustazioni gastronomiche a Sydney",
+    content: "Fai un viaggio serale nella scena emergente dei piccoli bar di Sydney e scopri i nuovi punti negli edifici storici e nei vicoli.",
+    "about": "Questo tour enogastronomico di Sydney ti darà un assaggio di ciò che la città ha da offrire: nel corso della serata, in un piccolo gruppo, visiterai tre diversi piccoli bar, tutti nascosti in scantinati, vicoli, vecchi strade carrabili, banchine di carico e altri angoli difficili da trovare. Trascorreremo circa 45 minuti in ogni sede, offrendoti un sacco di tempo per assaggiare degli snack e / o gustare un drink ed è raramente la stessa cosa due volte! Potremmo essere trattati per la pizza una volta o tapas o gnocchi un'altra volta, innaffiati magari con un bicchiere di vin brulè in inverno o sangria in estate. Vieni con una mente aperta per ciò che ti viene posto di fronte! Tra una sosta e l'altra, sarai guidato lungo alcune delle strade più interessanti di Sydney, dove potrai scoprire altri luoghi, vedere fantastici street art, spiare spazi intriganti e imparare dove la gente del posto va davvero dopo l'orario di lavoro. Inizieremo il nostro tour alla cupola di vetro di Wynyard Park e finiremo nella zona di Kent Street, dopo tre ore di esplorazione del centro città attraverso gli angoli nascosti nel centro di Sydney.",
+    cover_image_url: "https://images-sandbox.musement.com/cover/0003/06/thumb_205180_cover_header.jpeg?w=540"
 };
 
 export default function City() {
     return (
         <>
-<<<<<<< HEAD:pages/citta/[name]/index.jsx
-            <Header />
-            <HeroIntern title={data.name} description={data.content} bgImage={data.cover_image_url}/>
-=======
         <Layout>
-            <HeroIntern cityName={data.name} cityDescription={data.content} cityImage={data.cover_image_url}/>
->>>>>>> e2ebb0dadd0cb793d9ea86282ef2db4a41a79b08:pages/esperienze/[name]/index.jsx
+            <HeroIntern title={data.name} description={data.content} bgImage={data.cover_image_url}/>
             <Activities />
             <CitiesGrid />
         </Layout>

80264456e9e40c93304023cf09999b6a6ddc98e3
Dario Castiglione <dario.castiglione99@gmail.com>
2022-02-10 10:14:59 +0100

    rooting dinamico cities

--- pages/citta/[index]/index.jsx
+++ pages/citta/[index]/index.jsx
@@ -3,7 +3,8 @@ import Header from '../../../components/Header'
 import HeroIntern from "../../../components/HeroIntern";
 import Activities from '../../../components/Activities';
 import CitiesGrid from '../../../components/CitiesGrid';
-import Footer from '../../../components/Footer'
+import Footer from '../../../components/Footer';
+import { useRouter } from "next/router";
 
 //dati per esempio
 const data = {
@@ -12,11 +13,21 @@ const data = {
     cover_image_url: "https://images-sandbox.musement.com/cover/0002/15/amsterdam_header-114429.jpeg"
 };
 
-export default function City() {
+export default function City({ city }) {
+
+    const router = useRouter();
+
+    if(router.isFallback){
+        return <h1>loading</h1>
+      }
+
     return (
         <>
             <Header />
-            <HeroIntern cityName={data.name} cityDescription={data.content} cityImage={data.cover_image_url}/>
+            <HeroIntern 
+                cityName={city.name} 
+                cityDescription={city.content} 
+                cityImage={city.cover_image_url}/>
             <Activities />
             <CitiesGrid />
             <Footer />
@@ -24,3 +35,42 @@ export default function City() {
         </>
     )
 }
+export async function getStaticProps({ params }) {
+  
+    const res = await fetch(`https://sandbox.musement.com/api/v3/cities/${params.index}`);
+    const data = await res.json();
+  
+    if (!data) {
+      return {
+        notFound: true,
+      }
+    }
+
+    //console.log("genero pagina ")
+    return {
+      props: {
+        city: data,
+      },
+      revalidate: 10,
+    };
+  }
+  
+  export async function getStaticPaths(){
+    const res = await fetch("https://sandbox.musement.com/api/v3/cities");
+    const data = await res.json();
+    
+  
+    
+    const paths = data.map(city => {
+      return {
+        params: {
+          index: `${city.id}`
+        }
+      }
+    })
+  
+    return {
+      paths: paths,
+      fallback: true,
+    }
+  } 

28f3f3d35db723eb20a181cf101213c3be773ed2
Dario Castiglione <dario.castiglione99@gmail.com>
2022-02-10 11:50:52 +0100

    popoliazione cities page

--- components/CitiesGrid/index.jsx
+++ components/CitiesGrid/index.jsx
@@ -1,6 +1,7 @@
 
 import SimpleCard from '../SimpleCard';
 import styles from './CitiesGrid.module.scss';
+import Link from 'next/link';
 
 
 const CitiesGrid = (props) =>
@@ -9,11 +10,16 @@ const CitiesGrid = (props) =>
 
     return (
         <div className={styles.wrapper}>
-            {
-                cities.map((city, index) => 
-                    <SimpleCard key={index} text={city.name} />
-                )
-            }
+            
+
+               { cities.map((city, index) => 
+                <Link href={`citta/${city.id}`}>
+                    <a>
+                    <SimpleCard key={index} text={city.name} image={city.cover_image_url} />
+                    </a>
+                </Link>
+                )}
+            
         </div>
     );
 };
--- pages/citta/index.jsx
+++ pages/citta/index.jsx
@@ -1,16 +1,26 @@
-
-import Layout from "../../components/Layouts"
+import Layout from "../../components/Layouts";
 import HeroIntern from "../../components/HeroIntern";
-import CitiesGrid from '../../components/CitiesGrid';
+import CitiesGrid from "../../components/CitiesGrid";
 
+export async function getStaticProps() {
+  const res = await fetch("https://sandbox.musement.com/api/v3/cities");
+  const data = await res.json();
 
-export default function CitiesArchive() {
-    return(
-        <Layout>
-            
-            <HeroIntern title="città" description="Sei indeciso sulla tua prossima destinazione? Sfoglia il catalogo completo delle città e lasciati ispirare da TravelHub!"/>
-            <CitiesGrid data={[{},{},{},{},{},{}]}/>
-            
-        </Layout>
-    )
+  return {
+    props: {
+      cities: data,
+    },
+  };
+}
+
+export default function CitiesArchive({ cities }) {
+  return (
+    <Layout>
+      <HeroIntern
+        title="città"
+        description="Sei indeciso sulla tua prossima destinazione? Sfoglia il catalogo completo delle città e lasciati ispirare da TravelHub!"
+      />
+      <CitiesGrid data={cities} />
+    </Layout>
+  );
 }

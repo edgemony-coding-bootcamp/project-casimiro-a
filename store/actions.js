5b80cfbba4a7ddce419b170571d86775ddca6f46
Dario Castiglione <dario.castiglione99@gmail.com>
2022-02-11 16:05:43 +0100

    SearchBar

--- store/actions.js
+++ store/actions.js
@@ -11,3 +11,20 @@ export const setCarouselIndex= (num) => ({
     type: "setCarouselIndex",
     payload: num
 })
+export const SearchBarAppear= { type: "SearchBarAppear"}
+export const SearchBarDisappear= { type: "SearchBarDisappear"}
+
+const setSearchData = (data) => ({type:"setSearchData", payload: data}) 
+const showResult = ({type:"showResult", payload: true});
+export const hideResult = ({type:"hideResult", payload: false});
+export const SearchFetch = (e) => {
+
+ 
+        return(dispatch) => {
+        fetch(`https://sandbox.musement.com/api/v3/activities?text=${e.target.value}&text_operator=AUTO&extend_other_languages=AUTO&extend_content_fields=AUTO&fuzziness_level=LEVEL-0&zero_terms_query=NONE&limit=10&offset=0`)
+        .then(res => res.json())
+        .then(data => dispatch(setSearchData(data)))
+        .then(dispatch(showResult))
+        
+    }
+}

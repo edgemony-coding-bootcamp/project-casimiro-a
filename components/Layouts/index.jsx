526b27e93a2c1b29189f9b57639e8c6a0a1d21e4
Agnese Spinella <agnesespinella@gmail.com>
2022-02-11 11:39:13 +0100

    add navbar and deleted header

--- components/Layouts/index.jsx
+++ components/Layouts/index.jsx
@@ -1,10 +1,11 @@
-import Header from "../Header";
+
 import Footer from "../Footer";
+import NavBar from "../Navbar";
 
 export default function Layout({ children }) {
     return (
       <>
-        <Header />
+        <NavBar />
         {children}
         <Footer />
       </>

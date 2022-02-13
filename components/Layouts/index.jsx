b6a20c0e55329a3f0ebaf1fefc8f188d345fd80e
Dario Castiglione <dario.castiglione99@gmail.com>
2022-02-13 11:50:26 +0100

    fix Layout & remove next export from package.json

--- components/Layouts/index.jsx
+++ components/Layouts/index.jsx
@@ -1,6 +1,6 @@
 
 import Footer from "../Footer";
-import NavBar from "../Navbar";
+import NavBar from "../NavBar";
 import SideMenu from "../SideMenu";
 export default function Layout({ children }) {
     return (

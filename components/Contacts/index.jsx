33ef2058d3630f3a20cd61e43b12d013d3d1417b
Dario Castiglione <dario.castiglione99@gmail.com>
2022-02-18 20:16:34 +0100

    change all for in htmlFor

--- components/Contacts/index.jsx
+++ components/Contacts/index.jsx
@@ -37,9 +37,9 @@ export default function Contacts({showBtn}) {
                     <p>Iscriviti alla nostra newsletter per restare sempre aggiornato sulle stupende esperienze proposte da TravelHub</p>   
                     <div className={styles.newsletter}>
                         <input type="radio" name="newsletter" id="newsletter-si" value="si" />
-                        <label for="newsletter-si">Voglio iscrivermi!</label>
+                        <label htmlFor="newsletter-si">Voglio iscrivermi!</label>
                         <input type="radio" name="newsletter" id="newsletter-no" value="no" /> 
-                        <label for="newsletter-no">No, grazie.</label>
+                        <label htmlFor="newsletter-no">No, grazie.</label>
                     </div>
                 </div>
                 <div className={styles.contacts_form}>
@@ -47,29 +47,29 @@ export default function Contacts({showBtn}) {
                         <h3>Invia un messaggio</h3>
                         <div className={styles.input_row}>
                             <div className={styles.input_column}>
-                                <label for='name'> Nome</label>
+                                <label htmlFor='name'> Nome</label>
                                 <input type='text' name='name' placeholder='Nome' required/>
                             </div>
                             <div className={styles.input_column}>
-                                <label for='surname'> Cognome</label>
+                                <label htmlFor='surname'> Cognome</label>
                                 <input type='text' name='surname' placeholder='Cognome' required/>
                             </div>
                         </div>
                         <div className={styles.input_row}>
                             <div className={styles.input_column}>
-                                <label for='email'>Email</label>
+                                <label htmlFor='email'>Email</label>
                                 <input type='email' name='email' placeholder='Email' required/>
                             </div>
                             <div className={styles.input_column}>
-                                <label for='phone'> Telefono</label>
+                                <label htmlFor='phone'> Telefono</label>
                                 <input type='tel' name='phone' placeholder='Telefono'/>
                             </div>
                         </div>
-                        <label for="testoForm">Scrivi qui il tuo commento</label>
+                        <label htmlFor="testoForm">Scrivi qui il tuo commento</label>
                         <textarea name="testoForm" id="testoForm" placeholder="Scrivi qui il tuo commento" required></textarea>
                         <div>
                             <input type='checkbox' name='privacy' required/>
-                            <label for='privacy'> Autorizzo il trattamento dei miei dati personali in base all’art. 13 del D. Lgs. 196/2003
+                            <label htmlFor='privacy'> Autorizzo il trattamento dei miei dati personali in base all’art. 13 del D. Lgs. 196/2003
                             e all’art. 13 GDPR 679/16.*</label>
                         </div>
                         <button onClick={handleSubmit}>Invia</button>

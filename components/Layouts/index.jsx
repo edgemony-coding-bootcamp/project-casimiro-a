a238ad6752f82b5e2d7aa909d7a9f7632c262374
Federica Santoro <federica.santoro.1993@gmail.com>
2022-02-21 15:56:46 +0100

    CookiesModal-in-Layout

--- components/Layouts/index.jsx
+++ components/Layouts/index.jsx
@@ -1,14 +1,24 @@
-import BackToTop from "../BackToTop";
+import { useState, useEffect } from "react";
 import Footer from "../Footer";
 import NavBar from "../NavBar";
 import SideMenu from "../SideMenu";
+import CookiesModal from '../CookiesModal'; 
+
+
 export default function Layout({ children }) {
+
+    const [cookies, setCookies] = useState(true);
+
+    useEffect(() => {
+      window.addEventListener("load", () => setCookies(false));
+    }, []);
+
     return (
       <>
+        {cookies ? null :  <CookiesModal changeShowModal={() => setCookies(true)}/>} 
         <NavBar />
         <SideMenu />
         {children}
-        <BackToTop />
         <Footer />
       </>
     )

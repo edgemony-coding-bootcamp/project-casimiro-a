2de8011976f44ba2a924f3376e9a8b4af23d21fa
Federica Santoro <federica.santoro.1993@gmail.com>
2022-02-21 15:44:51 +0100

    feat:CookiesModal-in-Layout

--- components/CookiesModal/index.jsx
+++ components/CookiesModal/index.jsx
@@ -3,9 +3,10 @@ import { faCookie} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from "next/link";
 
-export default function CookiesModal() {
-  return (   
-       
+export default function CookiesModal(props) {
+  
+
+  return (          
     <div className={styles.modalWrapper}>
         <div className={styles.iconWrapper}>
             <FontAwesomeIcon className={styles.cookieIcons} icon={faCookie}/>
@@ -14,11 +15,7 @@ export default function CookiesModal() {
             <h3>Usiamo i cookies per offrirti un esperienza migliore.</h3>
         </div>
         <div className={styles.btns}>
-            <button className={styles.acceptBtn}>
-                <Link href="/">
-                    <a>Accetta</a>
-                </Link>
-            </button>
+            <button onClick={() => props.changeShowModal()} className={styles.acceptBtn}>Accetta</button>
             <button className={styles.denyBtn}>
                 <Link href="https://www.google.com">
                     <a target="_blank">Rifiuta</a>
--- components/Layouts/index.jsx
+++ components/Layouts/index.jsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from "react";
 import Footer from "../Footer";
 import NavBar from "../NavBar";
 import SideMenu from "../SideMenu";
+import CookiesModal from '../CookiesModal'; 
+
+
 export default function Layout({ children }) {
+
+    const [cookies, setCookies] = useState(true);
+
+    useEffect(() => {
+      window.addEventListener("load", () => setCookies(false));
+    }, []);
+
     return (
       <>
+        {cookies ? null :  <CookiesModal changeShowModal={() => setCookies(true)}/>} 
         <NavBar />
         <SideMenu />
         {children}
